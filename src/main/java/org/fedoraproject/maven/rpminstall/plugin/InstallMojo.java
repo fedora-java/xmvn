314c0710fde2378d08235b1f90d3d8d43bbe5952
Mikolaj Izdebski <mizdebsk@redhat.com>
2012-09-14 14:05:35 +0200

    Use annotations instead of javadoc tags

--- src/main/java/org/fedoraproject/maven/rpminstall/plugin/InstallMojo.java
+++ src/main/java/org/fedoraproject/maven/rpminstall/plugin/InstallMojo.java
@@ -26,25 +26,19 @@ import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugin.logging.Log;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 
-/**
- * @goal install
- */
+@Mojo(name = "install")
 public class InstallMojo extends AbstractMojo {
 
   private Log log;
   private String packageName;
   private String buildRoot;
 
-  /**
-   * POM
-   *
-   * @parameter expression="${project}"
-   * @readonly
-   * @required
-   */
-  protected MavenProject project;
+  @Parameter(defaultValue = "${project}", readonly = true, required = true)
+  private MavenProject project;
 
   private static String getenv(String key) throws MojoExecutionException {
     String val = System.getenv(key);
