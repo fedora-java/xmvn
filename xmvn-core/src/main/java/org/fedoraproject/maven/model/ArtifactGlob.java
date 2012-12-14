649f1aa72bdbb6a8663a469ee7223e9690133a4a
Mikolaj Izdebski <mizdebsk@redhat.com>
2012-12-14 15:37:05 +0100

    Implement creation of artifact from matched pattern

--- xmvn-core/src/main/java/org/fedoraproject/maven/model/ArtifactGlob.java
+++ xmvn-core/src/main/java/org/fedoraproject/maven/model/ArtifactGlob.java
@@ -15,6 +15,7 @@
  */
 package org.fedoraproject.maven.model;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.fedoraproject.maven.utils.GlobUtils;
@@ -22,24 +23,64 @@ import org.fedoraproject.maven.utils.StringSplitter;
 
 public class ArtifactGlob
 {
-    private final Pattern groupId;
+    private final Pattern groupIdPattern;
 
-    private final Pattern artifactId;
+    private final Pattern artifactIdPattern;
 
-    private final Pattern version;
+    private final Pattern versionPattern;
 
     public ArtifactGlob( String glob )
     {
         String[] tok = StringSplitter.split( glob, 3, ':' );
-        groupId = GlobUtils.glob2pattern( tok[0] );
-        artifactId = GlobUtils.glob2pattern( tok[1] );
-        version = GlobUtils.glob2pattern( tok[2] );
+        groupIdPattern = GlobUtils.glob2pattern( tok[0] );
+        artifactIdPattern = GlobUtils.glob2pattern( tok[1] );
+        versionPattern = GlobUtils.glob2pattern( tok[2] );
     }
 
     public boolean matches( Artifact artifact )
     {
-        return ( groupId == null || groupId.matcher( artifact.getGroupId() ).matches() )
-            && ( artifactId == null || artifactId.matcher( artifact.getArtifactId() ).matches() )
-            && ( version == null || version.matcher( artifact.getVersion() ).matches() );
+        return ( groupIdPattern == null || groupIdPattern.matcher( artifact.getGroupId() ).matches() )
+            && ( artifactIdPattern == null || artifactIdPattern.matcher( artifact.getArtifactId() ).matches() )
+            && ( versionPattern == null || versionPattern.matcher( artifact.getVersion() ).matches() );
+    }
+
+    private String match( String source, Pattern pattern, String target )
+    {
+        String result = source != null ? source : target;
+
+        if ( pattern != null )
+        {
+            Matcher matcher = pattern.matcher( target );
+            if ( !matcher.matches() )
+                return null;
+            if ( source != null )
+            {
+                for ( int group = 1; group <= matcher.groupCount(); group++ )
+                    result = result.replace( "@" + group, matcher.group( group ) );
+            }
+        }
+
+        return result.trim();
+    }
+
+    public Artifact createArtifactFromTemplate( Artifact artifact, String template )
+    {
+        String[] tok = StringSplitter.split( template, 3, ':' );
+
+        String groupId = match( tok[0], groupIdPattern, artifact.getGroupId() );
+        String artifactId = match( tok[1], artifactIdPattern, artifact.getArtifactId() );
+        String version = match( tok[2], versionPattern, artifact.getVersion() );
+
+        if ( groupId == null || artifactId == null || version == null )
+            return null;
+
+        if ( groupId.equals( "" ) )
+            groupId = null;
+        if ( artifactId.equals( "" ) )
+            artifactId = null;
+        if ( version.equals( "" ) )
+            version = null;
+
+        return new Artifact( groupId, artifactId, version );
     }
 }
