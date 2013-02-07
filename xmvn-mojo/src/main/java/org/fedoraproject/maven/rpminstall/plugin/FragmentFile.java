9d17b720aebc48a6ad9e5ebf9c06f93b1b744123
Michal Srb <msrb@redhat.com>
2013-02-07 09:03:41 +0100

    Skip blacklisted dependencies
    Signed-off-by: Mikolaj Izdebski <mizdebsk@redhat.com>

--- xmvn-mojo/src/main/java/org/fedoraproject/maven/rpminstall/plugin/FragmentFile.java
+++ xmvn-mojo/src/main/java/org/fedoraproject/maven/rpminstall/plugin/FragmentFile.java
@@ -23,6 +23,7 @@ import java.util.Iterator;
 import java.util.Set;
 import java.util.TreeSet;
 
+import org.fedoraproject.maven.ArtifactBlacklist;
 import org.fedoraproject.maven.Configuration;
 import org.fedoraproject.maven.model.Artifact;
 import org.fedoraproject.maven.resolver.DependencyMap;
@@ -125,6 +126,9 @@ public class FragmentFile
 
                 for ( Artifact dependency : combinedDependencies )
                 {
+                    if ( ArtifactBlacklist.contains( dependency ) )
+                        continue;
+
                     ps.println( "  <autoRequires>" );
                     ps.println( "    <groupId>" + dependency.getGroupId() + "</groupId>" );
                     ps.println( "    <artifactId>" + dependency.getArtifactId() + "</artifactId>" );
