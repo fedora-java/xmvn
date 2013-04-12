a12ca4c6548e646905817421a58ca9d97c4ea5b2
Mikolaj Izdebski <mizdebsk@redhat.com>
2013-04-12 15:09:06 +0200

    Improve javadocs for ProjectInstaller

--- xmvn-core/src/main/java/org/fedoraproject/maven/installer/ProjectInstaller.java
+++ xmvn-core/src/main/java/org/fedoraproject/maven/installer/ProjectInstaller.java
@@ -22,12 +22,38 @@ import org.apache.maven.project.MavenProject;
 import org.fedoraproject.maven.config.PackagingRule;
 
 /**
+ * Component that can install some kinds of Maven artifacts.
+ * <p>
+ * There are several Plexus components implementing this interface. Each of the components is responsible for installing
+ * some kind of artifacts.
+ * <p>
+ * Users of this class should can either iterate over all project installer components available in Plexus container and
+ * call {@code getSupportedPackagingType()} to see which components are suitable for installing which projects, or use
+ * role hint to lookup the right component.
+ * 
  * @author Mikolaj Izdebski
  */
 public interface ProjectInstaller
 {
+    /**
+     * Get list of supported packaging types.
+     * 
+     * @return list of packaging types supported by this object
+     */
     List<String> getSupportedPackagingTypes();
 
+    /**
+     * Install Maven project into binary package.
+     * <p>
+     * This method can be called only if project packaging type is on the list of supported packaging types, as returned
+     * by {@code getSupportedPackagingTypes()} method.
+     * 
+     * @param project Maven project to be installed
+     * @param targetPackage binary package into which project should be installed
+     * @param rule effective packaging rule to use
+     * @throws ProjectInstallationException if project cannot be installed for some reason
+     * @throws IOException if I/O error occurs when installing package files
+     */
     void installProject( MavenProject project, Package targetPackage, PackagingRule rule )
         throws ProjectInstallationException, IOException;
 }
