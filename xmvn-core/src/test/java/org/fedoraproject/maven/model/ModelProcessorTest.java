51fbdf973a16313deb28c328a49dd40c7280fd11
Mikolaj Izdebski <mizdebsk@redhat.com>
2013-05-28 15:13:41 +0200

    Implement ModelProcessorTest.testInformationRetrieval()

--- xmvn-core/src/test/java/org/fedoraproject/maven/model/ModelProcessorTest.java
+++ xmvn-core/src/test/java/org/fedoraproject/maven/model/ModelProcessorTest.java
@@ -15,7 +15,12 @@
  */
 package org.fedoraproject.maven.model;
 
+import java.util.concurrent.Semaphore;
+
+import org.apache.maven.model.Dependency;
+import org.apache.maven.model.Exclusion;
 import org.apache.maven.model.Model;
+import org.apache.maven.model.Profile;
 import org.codehaus.plexus.PlexusTestCase;
 
 /**
@@ -46,5 +51,70 @@ public class ModelProcessorTest
     {
         ModelProcessor processor = lookup( ModelProcessor.class );
         processor.processModel( new Model(), new AbstractModelVisitor() );
+    }
+
+    /**
+     * Test if processor can retrieve information.
+     * 
+     * @throws Exception
+     */
+    public void testInformationRetrieval()
+        throws Exception
+    {
+        Model model = new Model();
+
+        Profile profile = new Profile();
+        model.addProfile( profile );
+
+        Dependency dependency = new Dependency();
+        profile.addDependency( dependency );
+
+        Exclusion exclusion = new Exclusion();
+        exclusion.setArtifactId( "foo" );
+        dependency.addExclusion( exclusion );
+
+        Dependency dependency2 = new Dependency();
+        model.addDependency( dependency2 );
+
+        Exclusion exclusion2 = new Exclusion();
+        exclusion2.setArtifactId( "bar" );
+        dependency2.addExclusion( exclusion2 );
+
+        final Semaphore fooSemaphore = new Semaphore( 0 );
+        final Semaphore barSemaphore = new Semaphore( 0 );
+
+        ModelProcessor processor = lookup( ModelProcessor.class );
+        processor.processModel( model, new AbstractModelVisitor()
+        {
+            @Override
+            public void visitDependencyExclusion( Exclusion exclusion )
+            {
+                assertEquals( "bar", exclusion.getArtifactId() );
+                barSemaphore.release();
+            }
+
+            @Override
+            public void visitProfileDependencyExclusion( Exclusion exclusion )
+            {
+                assertEquals( "foo", exclusion.getArtifactId() );
+                fooSemaphore.release();
+            }
+
+            @Override
+            public void visitBuildPluginDependencyExclusion( Exclusion exclusion )
+            {
+                fail();
+            }
+
+            @Override
+            public void visitBuildPluginManagementPluginDependencyExclusion( Exclusion exclusion )
+            {
+                fail();
+            }
+
+        } );
+
+        assertEquals( 1, fooSemaphore.availablePermits() );
+        assertEquals( 1, barSemaphore.availablePermits() );
     }
 }
