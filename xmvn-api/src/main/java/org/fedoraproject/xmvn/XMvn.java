39c54167704bea383e4ca274aa218b17e7fb156a
Mikolaj Izdebski <mizdebsk@redhat.com>
2014-02-04 12:59:46 +0100

    Remove old class loader isolation code

--- xmvn-api/src/main/java/org/fedoraproject/xmvn/XMvn.java
+++ xmvn-api/src/main/java/org/fedoraproject/xmvn/XMvn.java
@@ -15,24 +15,6 @@
  */
 package org.fedoraproject.xmvn;
 
-import java.io.IOException;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.net.URL;
-import java.net.URLClassLoader;
-import java.nio.file.DirectoryIteratorException;
-import java.nio.file.DirectoryStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
-import org.eclipse.aether.artifact.Artifact;
-import org.eclipse.aether.artifact.DefaultArtifact;
 import org.fedoraproject.xmvn.deployer.Deployer;
 import org.fedoraproject.xmvn.deployer.DeploymentRequest;
 import org.fedoraproject.xmvn.deployer.DeploymentResult;
@@ -45,162 +27,14 @@ import org.fedoraproject.xmvn.resolver.Resolver;
  */
 public class XMvn
 {
-    private static Path getHome()
-    {
-        String home = System.getProperty( "xmvn.home" );
-        if ( home == null )
-            home = System.getenv( "XMVN_HOME" );
-        if ( home == null )
-            home = System.getenv( "M2_HOME" );
-        if ( home == null )
-            home = "/usr/share/xmvn";
-        return Paths.get( home );
-    }
-
-    private static URL[] getLibraryJars()
-    {
-        List<URL> urls = new ArrayList<>();
-
-        for ( String subdir : Arrays.asList( "boot", "lib/ext", "lib" ) )
-        {
-            Path dir = getHome().resolve( subdir );
-            try (DirectoryStream<Path> stream = Files.newDirectoryStream( dir ))
-            {
-                for ( Path file : stream )
-                {
-                    if ( file.toString().endsWith( ".jar" ) )
-                        urls.add( file.toUri().toURL() );
-                }
-            }
-            catch ( IOException | DirectoryIteratorException e )
-            {
-                throw new RuntimeException( e );
-            }
-        }
-
-        return urls.toArray( new URL[0] );
-    }
-
-    private static ClassLoader savedClassLoader;
-
-    private static ClassLoader sisuRealm;
-
-    private static void enterSisuRealm()
-    {
-        Thread currentThread = Thread.currentThread();
-        savedClassLoader = currentThread.getContextClassLoader();
-
-        if ( sisuRealm == null )
-            sisuRealm = new URLClassLoader( getLibraryJars(), XMvn.class.getClassLoader() );
-        currentThread.setContextClassLoader( sisuRealm );
-    }
-
-    private static void leaveSisuRealm()
-    {
-        Thread.currentThread().setContextClassLoader( savedClassLoader );
-    }
-
-    @SuppressWarnings( { "rawtypes", "unchecked" } )
-    private static <T> T lookup( Class<T> role )
-    {
-        try
-        {
-            enterSisuRealm();
-
-            Class urlClassSpaceClass = sisuRealm.loadClass( "org.eclipse.sisu.space.URLClassSpace" );
-            Class spaceModuleClass = sisuRealm.loadClass( "org.eclipse.sisu.space.SpaceModule" );
-            Class classSpaceClass = sisuRealm.loadClass( "org.eclipse.sisu.space.ClassSpace" );
-            Class wireModuleClass = sisuRealm.loadClass( "org.eclipse.sisu.wire.WireModule" );
-            Class guiceClass = sisuRealm.loadClass( "com.google.inject.Guice" );
-            Class injectorClass = sisuRealm.loadClass( "com.google.inject.Injector" );
-
-            Constructor urlClassSpaceConstructor = urlClassSpaceClass.getConstructor( ClassLoader.class );
-            Constructor spaceModuleConstructor = spaceModuleClass.getConstructor( classSpaceClass );
-            Constructor wireModuleConstructor = wireModuleClass.getConstructor( Iterable.class );
-            Method createInjectorMethod = guiceClass.getMethod( "createInjector", Iterable.class );
-            Method getInstanceMethod = injectorClass.getMethod( "getInstance", Class.class );
-
-            Object urlClassSpace = urlClassSpaceConstructor.newInstance( sisuRealm );
-            Object spaceModule = spaceModuleConstructor.newInstance( urlClassSpace );
-            Object wireModule = wireModuleConstructor.newInstance( Collections.singleton( spaceModule ) );
-            Object injector = createInjectorMethod.invoke( null, Collections.singleton( wireModule ) );
-            Object component = getInstanceMethod.invoke( injector, role );
-
-            return (T) component;
-        }
-        catch ( ClassNotFoundException | NoSuchMethodException | SecurityException | IllegalAccessException
-                        | IllegalArgumentException | InvocationTargetException | InstantiationException e )
-        {
-            throw new RuntimeException( e );
-        }
-        finally
-        {
-            leaveSisuRealm();
-        }
-    }
-
-    private static class IsolatedResolver
-        implements Resolver
-    {
-        private static final Resolver instance = new IsolatedResolver();
-
-        private final Resolver delegate;
-
-        public IsolatedResolver()
-        {
-            delegate = lookup( Resolver.class );
-        }
-
-        @Override
-        public ResolutionResult resolve( ResolutionRequest request )
-        {
-            try
-            {
-                enterSisuRealm();
-                return delegate.resolve( request );
-            }
-            finally
-            {
-                leaveSisuRealm();
-            }
-        }
-    }
-
-    private static class IsolatedDeployer
-        implements Deployer
-    {
-        private static final Deployer instance = new IsolatedDeployer();
-
-        private final Deployer delegate;
-
-        public IsolatedDeployer()
-        {
-            delegate = lookup( Deployer.class );
-        }
-
-        @Override
-        public DeploymentResult deploy( DeploymentRequest request )
-        {
-            try
-            {
-                enterSisuRealm();
-                return delegate.deploy( request );
-            }
-            finally
-            {
-                leaveSisuRealm();
-            }
-        }
-    }
-
     public static Resolver getResolver()
     {
-        return IsolatedResolver.instance;
+        return IsolatedResolver.INSTANCE;
     }
 
     public static Deployer getDeployer()
     {
-        return IsolatedDeployer.instance;
+        return IsolatedDeployer.INSTANCE;
     }
 
     public static ResolutionResult resolve( ResolutionRequest request )
@@ -211,13 +45,5 @@ public class XMvn
     public static DeploymentResult deploy( DeploymentRequest request )
     {
         return getDeployer().deploy( request );
-    }
-
-    public static void main( String[] args )
-    {
-        Artifact artifact = new DefaultArtifact( args[0] );
-        ResolutionRequest request = new ResolutionRequest( artifact );
-        ResolutionResult result = resolve( request );
-        System.err.println( result.getArtifactFile() );
     }
 }
