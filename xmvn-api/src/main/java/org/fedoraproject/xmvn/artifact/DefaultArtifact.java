f119943e7820d3e57a6b157fb623857937819cfc
Mikolaj Izdebski <mizdebsk@redhat.com>
2014-03-12 16:44:06 +0100

    Implement artifact adapter

---
+++ xmvn-api/src/main/java/org/fedoraproject/xmvn/artifact/DefaultArtifact.java
@@ -0,0 +1,115 @@
+/*-
+ * Copyright (c) 2014 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.fedoraproject.xmvn.artifact;
+
+import java.io.File;
+import java.util.Map;
+
+/**
+ * @author Mikolaj Izdebski
+ */
+public final class DefaultArtifact
+    implements Artifact
+{
+    private final org.eclipse.aether.artifact.Artifact artifact;
+
+    private DefaultArtifact( org.eclipse.aether.artifact.Artifact artifact )
+    {
+        this.artifact = artifact;
+    }
+
+    public DefaultArtifact( String coords )
+    {
+        artifact = new org.eclipse.aether.artifact.DefaultArtifact( coords );
+    }
+
+    public DefaultArtifact( String groupId, String artifactId, String extension, String version )
+    {
+        artifact = new org.eclipse.aether.artifact.DefaultArtifact( groupId, artifactId, extension, version );
+    }
+
+    public DefaultArtifact( String groupId, String artifactId, String classifier, String extension, String version )
+    {
+        artifact =
+            new org.eclipse.aether.artifact.DefaultArtifact( groupId, artifactId, classifier, extension, version );
+    }
+
+    @Override
+    public String getGroupId()
+    {
+        return artifact.getGroupId();
+    }
+
+    @Override
+    public String getArtifactId()
+    {
+        return artifact.getArtifactId();
+    }
+
+    @Override
+    public String getExtension()
+    {
+        return artifact.getExtension();
+    }
+
+    @Override
+    public String getClassifier()
+    {
+        return artifact.getClassifier();
+    }
+
+    @Override
+    public String getVersion()
+    {
+        return artifact.getVersion();
+    }
+
+    @Override
+    public File getFile()
+    {
+        return artifact.getFile();
+    }
+
+    @Override
+    public Artifact setVersion( String version )
+    {
+        return new DefaultArtifact( artifact.setVersion( version ) );
+    }
+
+    @Override
+    public Artifact setFile( File file )
+    {
+        return new DefaultArtifact( artifact.setFile( file ) );
+    }
+
+    @Override
+    public String getProperty( String key, String defaultValue )
+    {
+        return getProperty( key, defaultValue );
+    }
+
+    @Override
+    public Map<String, String> getProperties()
+    {
+        return getProperties();
+    }
+
+    @Override
+    public Artifact setProperties( Map<String, String> properties )
+    {
+        return new DefaultArtifact( artifact.setProperties( properties ) );
+    }
+}
