7071d44f7086b6c52d158c49dfa78c4c23ac1b8b
Mikolaj Izdebski <mizdebsk@redhat.com>
2014-04-01 19:43:28 +0200

    Implement DefaultResolver

---
+++ xmvn-core/src/main/java/org/fedoraproject/xmvn/resolver/impl/DefaultResolver.java
@@ -0,0 +1,93 @@
+/*-
+ * Copyright (c) 2014 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.fedoraproject.xmvn.resolver.impl;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.fedoraproject.xmvn.artifact.Artifact;
+import org.fedoraproject.xmvn.metadata.ArtifactMetadata;
+import org.fedoraproject.xmvn.resolver.ResolutionRequest;
+import org.fedoraproject.xmvn.resolver.ResolutionResult;
+import org.fedoraproject.xmvn.resolver.Resolver;
+import org.fedoraproject.xmvn.utils.FileUtils;
+
+/**
+ * Default implementation of XMvn {@code Resolver} interface.
+ * <p>
+ * <strong>WARNING</strong>: This class is part of internal implementation of XMvn and it is marked as public only for
+ * technical reasons. This class is not part of XMvn API. Client code using XMvn should <strong>not</strong> reference
+ * it directly.
+ * 
+ * @author Mikolaj Izdebski
+ */
+@Named
+@Singleton
+public class DefaultResolver
+    implements Resolver
+{
+    private final Logger logger = LoggerFactory.getLogger( DefaultResolver.class );
+
+    private MetadataResolver metadataResolver;
+
+    private static final RpmDb rpmdb = new RpmDb();
+
+    @Override
+    public ResolutionResult resolve( ResolutionRequest request )
+    {
+        // FIXME: bisect is not used
+        Artifact artifact = request.getArtifact();
+        logger.debug( "Trying to resolve artifact {}", artifact );
+
+        String compatVersion;
+        ArtifactMetadata metadata = metadataResolver.resolveArtifactMetadata( artifact );
+
+        if ( metadata == null )
+        {
+            metadata = metadataResolver.resolveArtifactMetadata( artifact.setVersion( Artifact.DEFAULT_VERSION ) );
+            compatVersion = null;
+        }
+        else
+        {
+            compatVersion = artifact.getVersion();
+        }
+
+        if ( metadata != null )
+        {
+            // TODO: interpolate paths to expand variables like Java home
+            Path artifactPath = Paths.get( metadata.getPath() );
+            artifactPath = FileUtils.followSymlink( artifactPath );
+
+            DefaultResolutionResult result = new DefaultResolutionResult( artifactPath );
+            result.setNamespace( metadata.getNamespace() );
+            result.setCompatVersion( compatVersion );
+            if ( request.isProviderNeeded() )
+                result.setProvider( rpmdb.lookupPath( artifactPath ) );
+
+            logger.debug( "Artifact {} was resolved to {}", artifact, artifactPath );
+            return result;
+        }
+
+        logger.debug( "Failed to resolve artifact: {}", artifact );
+        return new DefaultResolutionResult();
+    }
+}
