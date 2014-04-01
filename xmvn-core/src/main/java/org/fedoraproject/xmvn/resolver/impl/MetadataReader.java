40a867901975dfbb62933718903dbb271ca03b3a
Mikolaj Izdebski <mizdebsk@redhat.com>
2014-04-01 17:33:19 +0200

    Add MetadataReader

---
+++ xmvn-core/src/main/java/org/fedoraproject/xmvn/resolver/impl/MetadataReader.java
@@ -0,0 +1,161 @@
+/*-
+ * Copyright (c) 2014 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.fedoraproject.xmvn.resolver.impl;
+
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.zip.GZIPInputStream;
+
+import org.fedoraproject.xmvn.metadata.Metadata;
+import org.fedoraproject.xmvn.metadata.io.stax.MetadataStaxReader;
+
+/**
+ * @author Mikolaj Izdebski
+ */
+class MetadataReader
+{
+    private final ThreadPoolExecutor executor;
+
+    static class DaemonFactory
+        implements ThreadFactory
+    {
+        @Override
+        public Thread newThread( Runnable runnable )
+        {
+            Thread thread = new Thread( runnable );
+            thread.setName( MetadataReader.class.getCanonicalName() + ".worker" );
+            thread.setDaemon( true );
+            return thread;
+        }
+    }
+
+    public MetadataReader()
+    {
+        BlockingQueue<Runnable> queue = new LinkedBlockingQueue<>();
+        int nThread = 2 * Math.min( Math.max( Runtime.getRuntime().availableProcessors(), 1 ), 8 );
+        executor = new ThreadPoolExecutor( nThread, nThread, 1, TimeUnit.MINUTES, queue, new DaemonFactory() );
+    }
+
+    public List<Metadata> readMetadata( List<Path> depmapLocations )
+    {
+        List<Future<Metadata>> futures = new ArrayList<>();
+
+        for ( Path path : depmapLocations )
+        {
+            if ( Files.isDirectory( path ) )
+            {
+                String flist[] = path.toFile().list();
+                if ( flist != null )
+                {
+                    Arrays.sort( flist );
+                    for ( String fragFilename : flist )
+                        futures.add( executor.submit( new Task( path.resolve( fragFilename ) ) ) );
+                }
+            }
+            else
+            {
+                futures.add( executor.submit( new Task( path ) ) );
+            }
+        }
+
+        try
+        {
+            List<Metadata> result = new ArrayList<>();
+
+            for ( Future<Metadata> future : futures )
+            {
+                try
+                {
+                    result.add( future.get() );
+                }
+                catch ( ExecutionException e )
+                {
+                    // Ignore. Failure to read metadata of a single package should not break the whole system
+                }
+            }
+
+            return result;
+        }
+        catch ( InterruptedException e )
+        {
+            throw new RuntimeException( e );
+        }
+    }
+
+    class Task
+        implements Callable<Metadata>
+    {
+        private final Path path;
+
+        public Task( Path path )
+        {
+            this.path = path;
+        }
+
+        @Override
+        public Metadata call()
+            throws Exception
+        {
+            try (InputStream fis = Files.newInputStream( path ))
+            {
+                try (BufferedInputStream bis = new BufferedInputStream( fis, 128 ))
+                {
+                    try (InputStream is = isCompressed( bis ) ? new GZIPInputStream( bis ) : bis)
+                    {
+                        MetadataStaxReader reader = new MetadataStaxReader();
+                        return reader.read( is );
+                    }
+                }
+            }
+        }
+
+        private boolean isCompressed( BufferedInputStream bis )
+            throws IOException
+        {
+            try
+            {
+                bis.mark( 2 );
+                DataInputStream ois = new DataInputStream( bis );
+                int magic = Short.reverseBytes( ois.readShort() ) & 0xFFFF;
+                return magic == GZIPInputStream.GZIP_MAGIC;
+            }
+            catch ( EOFException e )
+            {
+                return false;
+            }
+            finally
+            {
+                bis.reset();
+            }
+        }
+    }
+}
