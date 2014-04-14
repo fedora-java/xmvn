feb4b7379a8d3d9ffdb3a94796e355d8f4e9ac83
Mikolaj Izdebski <mizdebsk@redhat.com>
2014-04-14 17:55:02 +0200

    Add installation methods for File

--- xmvn-tools/xmvn-install/src/main/java/org/fedoraproject/xmvn/tools/install/impl/File.java
+++ xmvn-tools/xmvn-install/src/main/java/org/fedoraproject/xmvn/tools/install/impl/File.java
@@ -15,6 +15,8 @@
  */
 package org.fedoraproject.xmvn.tools.install.impl;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 /**
@@ -37,6 +39,18 @@ abstract class File
     private final int accessMode;
 
     /**
+     * Install the file into specified location.
+     * <p>
+     * Implementations of this method can assume that all parent directory of target file already exists. Access mode of
+     * target file doesn't have to be set as it will be manipulated with other means.
+     * 
+     * @param installRoot absolute path of root directory under which the file should be installed
+     * @throws IOException
+     */
+    protected abstract void installContents( Path installRoot )
+        throws IOException;
+
+    /**
      * Create a file object with specified path and default access mode of 0644.
      * 
      * @param targetPath
@@ -54,6 +68,11 @@ abstract class File
      */
     public File( Path targetPath, int accessMode )
     {
+        if ( targetPath.isAbsolute() )
+            throw new IllegalArgumentException( "target path must not be absolute" );
+        if ( accessMode < 0 || accessMode > 0777 )
+            throw new IllegalArgumentException( "access mode must be in range from 0 to 777" );
+
         this.targetPath = targetPath;
         this.accessMode = accessMode;
     }
@@ -76,5 +95,32 @@ abstract class File
     public int getAccessMode()
     {
         return accessMode;
+    }
+
+    /**
+     * Install file into specified root directory.
+     * 
+     * @param installRoot
+     * @throws IOException
+     */
+    public void install( Path installRoot )
+        throws IOException
+    {
+        installRoot = installRoot.toAbsolutePath();
+
+        if ( Files.exists( installRoot ) )
+        {
+            if ( !Files.isDirectory( installRoot ) )
+                throw new IOException( "Installation root " + installRoot + " already exists and is not a directory." );
+        }
+        else
+        {
+            Files.createDirectory( installRoot );
+        }
+
+        Path targetAbsolutePath = installRoot.resolve( targetPath );
+        Files.createDirectories( targetAbsolutePath.getParent() );
+
+        installContents( installRoot );
     }
 }
