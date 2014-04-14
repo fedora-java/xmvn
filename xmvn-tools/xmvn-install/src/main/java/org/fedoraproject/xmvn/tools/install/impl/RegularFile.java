6261d83d8d3524c1d9a3d3d299f21b9ce123b386
Mikolaj Izdebski <mizdebsk@redhat.com>
2014-04-14 18:35:51 +0200

    Implement RegularFile class

---
+++ xmvn-tools/xmvn-install/src/main/java/org/fedoraproject/xmvn/tools/install/impl/RegularFile.java
@@ -0,0 +1,118 @@
+/*-
+ * Copyright (c) 2014 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.fedoraproject.xmvn.tools.install.impl;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+/**
+ * A regular file created installed in target package.
+ * <p>
+ * The file can be installed either by coping an existing file (source file), or by writing provided contents.
+ * 
+ * @author Mikolaj Izdebski
+ */
+class RegularFile
+    extends File
+{
+    /**
+     * Path to source file which contents will be copied to create target file. Can be {@code null}, in which case byte
+     * contents are used instead.
+     */
+    private final Path sourcePath;
+
+    /**
+     * Array of bytes using which contents of target files will be populated. It is used only if source path is not
+     * provided (is {@code null}).
+     */
+    private final byte[] content;
+
+    /**
+     * Create a regular file object, which contents will be populated from a source file. Target file will have default
+     * access mode (0644).
+     * 
+     * @param targetPath file path, relative to installation root
+     * @param sourcePath path to source file which will be copied to target path
+     */
+    public RegularFile( Path targetPath, Path sourcePath )
+    {
+        this( targetPath, sourcePath, 0644 );
+    }
+
+    /**
+     * Create a regular file object, which contents will be populated from a byte array. Target file will have default
+     * access mode (0644).
+     * 
+     * @param targetPath file path, relative to installation root
+     * @param content array of bytes using to populate target file contents with
+     */
+    public RegularFile( Path targetPath, byte[] content )
+    {
+        this( targetPath, content, 0644 );
+    }
+
+    /**
+     * Create a regular file object, which contents will be populated from a source file. Target file will have
+     * specified access mode.
+     * 
+     * @param targetPath file path, relative to installation root
+     * @param sourcePath path to source file which will be copied to target path
+     * @param accessMode Unix access mode of the file (must be an integer in range from 0 to 0777)
+     */
+    public RegularFile( Path targetPath, Path sourcePath, int accessMode )
+    {
+        super( targetPath, accessMode );
+
+        this.sourcePath = sourcePath;
+        this.content = null;
+    }
+
+    /**
+     * Create a regular file object, which contents will be populated from a byte array.Target file will have specified
+     * access mode
+     * 
+     * @param targetPath file path, relative to installation root
+     * @param content array of bytes using to populate target file contents with
+     * @param accessMode Unix access mode of the file (must be an integer in range from 0 to 0777)
+     */
+    public RegularFile( Path targetPath, byte[] content, int accessMode )
+    {
+        super( targetPath, accessMode );
+
+        this.sourcePath = null;
+        this.content = content;
+    }
+
+    @Override
+    protected void installContents( Path targetPath )
+        throws IOException
+    {
+        if ( sourcePath != null )
+        {
+            Files.copy( sourcePath, targetPath );
+        }
+        else
+        {
+            try (InputStream stream = new ByteArrayInputStream( content ))
+            {
+                Files.copy( stream, targetPath );
+            }
+        }
+    }
+}
