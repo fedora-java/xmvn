fb090d095ce4a6d89ab94851844a294f4a26ea58
Michael Simacek <msimacek@redhat.com>
2014-04-22 14:06:47 +0200

    [test] More tests for DefaultArtifactInstaller

--- xmvn-tools/xmvn-install/src/test/java/org/fedoraproject/xmvn/tools/install/impl/DefaultArtifactInstallerTest.java
+++ xmvn-tools/xmvn-install/src/test/java/org/fedoraproject/xmvn/tools/install/impl/DefaultArtifactInstallerTest.java
@@ -20,9 +20,13 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 import org.fedoraproject.xmvn.artifact.Artifact;
 import org.fedoraproject.xmvn.config.PackagingRule;
+import org.fedoraproject.xmvn.metadata.ArtifactAlias;
 import org.fedoraproject.xmvn.metadata.ArtifactMetadata;
 import org.fedoraproject.xmvn.metadata.PackageMetadata;
 import org.fedoraproject.xmvn.repository.Repository;
@@ -141,26 +145,98 @@ public class DefaultArtifactInstallerTest
         }
     } );
 
-    @Test
-    public void testInstallation()
-            throws Exception
+    private ArtifactMetadata createArtifact()
     {
         ArtifactMetadata artifact = new ArtifactMetadata();
         artifact.setGroupId( "com.example" );
         artifact.setArtifactId( "test" );
         artifact.setVersion( "4.5" );
         artifact.setPath( getResource( "example.jar" ).toString() );
+        return artifact;
+    }
 
-        JavaPackage pkg = new JavaPackage( "test", Paths.get( "usr/share/maven-metadata/test.xml" ) );
-
+    @Test
+    public void testInstallation()
+            throws Exception
+    {
+        ArtifactMetadata artifact = createArtifact();
+        Path metadataPath = Paths.get( "usr/share/maven-metadata/test.xml" );
+        JavaPackage pkg = new JavaPackage( "test", metadataPath );
         PackagingRule rule = new PackagingRule();
 
         DefaultArtifactInstaller installer = injector.getInstance( DefaultArtifactInstaller.class );
         installer.install( pkg, artifact, rule );
 
         PackageMetadata metadata = pkg.getMetadata();
+        assertEquals( 1, metadata.getArtifacts().size() );
         ArtifactMetadata actualArtifact = metadata.getArtifacts().get( 0 );
         assertEquals( "ns", actualArtifact.getNamespace() );
         assertNotNull( actualArtifact.getUuid() );
+
+        assertEquals( 2, pkg.getFiles().size() );
+        Iterator<File> iterator = pkg.getFiles().iterator();
+        File file = iterator.next();
+        if ( file.getTargetPath().equals( metadataPath ) )
+            file = iterator.next();
+        assertEquals( Paths.get( "com.example-test" ), file.getTargetPath() );
+        assertEquals( "/com.example-test", artifact.getPath() );
+    }
+
+    @Test
+    public void testCompatVersion()
+            throws Exception
+    {
+        ArtifactMetadata artifact = createArtifact();
+        JavaPackage pkg = new JavaPackage( "test", Paths.get( "usr/share/maven-metadata/test.xml" ) );
+        PackagingRule rule = new PackagingRule();
+        rule.addVersion( "3.4" );
+        rule.addVersion( "3" );
+
+        DefaultArtifactInstaller installer = injector.getInstance( DefaultArtifactInstaller.class );
+        installer.install( pkg, artifact, rule );
+
+        PackageMetadata metadata = pkg.getMetadata();
+        assertEquals( 1, metadata.getArtifacts().size() );
+        ArtifactMetadata actualArtifact = metadata.getArtifacts().get( 0 );
+        List<String> actualVersions = actualArtifact.getCompatVersions();
+        Collections.sort( actualVersions );
+        assertEquals( Arrays.asList( new String[]
+        {
+            "3", "3.4"
+        } ), actualVersions );
+    }
+
+    @Test
+    public void testAliases()
+            throws Exception
+    {
+        ArtifactMetadata artifact = createArtifact();
+        JavaPackage pkg = new JavaPackage( "test", Paths.get( "usr/share/maven-metadata/test.xml" ) );
+        PackagingRule rule = new PackagingRule();
+
+        org.fedoraproject.xmvn.config.Artifact alias1 = new org.fedoraproject.xmvn.config.Artifact();
+        alias1.setGroupId( "com.example" );
+        alias1.setArtifactId( "alias1" );
+        alias1.setVersion( "3.4" );
+        org.fedoraproject.xmvn.config.Artifact alias2 = new org.fedoraproject.xmvn.config.Artifact();
+        alias2.setGroupId( "com.example" );
+        alias2.setArtifactId( "alias2" );
+        alias2.setClassifier( "war" );
+        rule.addAlias( alias1 );
+        rule.addAlias( alias2 );
+
+        DefaultArtifactInstaller installer = injector.getInstance( DefaultArtifactInstaller.class );
+        installer.install( pkg, artifact, rule );
+
+        PackageMetadata metadata = pkg.getMetadata();
+        assertEquals( 1, metadata.getArtifacts().size() );
+        ArtifactMetadata actualArtifact = metadata.getArtifacts().get( 0 );
+        List<ArtifactAlias> actualAliases = actualArtifact.getAliases();
+        assertEquals( 2, actualAliases.size() );
+        assertEquals( "com.example", actualAliases.get( 0 ).getGroupId() );
+        assertEquals( "alias1", actualAliases.get( 0 ).getArtifactId() );
+        assertEquals( "com.example", actualAliases.get( 1 ).getGroupId() );
+        assertEquals( "alias2", actualAliases.get( 1 ).getArtifactId() );
+        assertEquals( "war", actualAliases.get( 1 ).getClassifier() );
     }
 }
