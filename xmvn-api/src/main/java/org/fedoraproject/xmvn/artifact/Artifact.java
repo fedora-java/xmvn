cbd46c8c2bc3c39bc224e66148f866428f398491
Mikolaj Izdebski <mizdebsk@redhat.com>
2014-07-29 10:16:05 +0200

    Improve javadocs for Artifact interface

--- xmvn-api/src/main/java/org/fedoraproject/xmvn/artifact/Artifact.java
+++ xmvn-api/src/main/java/org/fedoraproject/xmvn/artifact/Artifact.java
@@ -18,27 +18,86 @@ package org.fedoraproject.xmvn.artifact;
 import java.nio.file.Path;
 
 /**
+ * An abstract entity uniquely identified by its coordinates &ndash; group identifier, artifact identifier, extension,
+ * classifier and version, with optionally associated artifact file.
+ * <p>
+ * Artifact objects immutable &ndash; all methods which modify artifact return a new object and keep the original
+ * unmodified.
+ * 
  * @author Mikolaj Izdebski
  */
 public interface Artifact
 {
-    public static final String DEFAULT_EXTENSION = "jar";
+    /**
+     * Default artifact extension, used if no explicit extension is specified.
+     */
+    static final String DEFAULT_EXTENSION = "jar";
 
-    public static final String DEFAULT_VERSION = "SYSTEM";
+    /**
+     * Default artifact version, used if no explicit version is specified.
+     */
+    static final String DEFAULT_VERSION = "SYSTEM";
 
+    /**
+     * Get group identifier of this artifact.
+     * 
+     * @return artifact group identifier, never {@code null}.
+     */
     String getGroupId();
 
+    /**
+     * Get artifact identifier of this artifact.
+     * 
+     * @return artifact identifier, never {@code null}.
+     */
     String getArtifactId();
 
+    /**
+     * Get extension of this artifact.
+     * 
+     * @return artifact extension, never {@code null}.
+     */
     String getExtension();
 
+    /**
+     * Get classifier of this artifact.
+     * 
+     * @return artifact classifier, never {@code null}.
+     */
     String getClassifier();
 
+    /**
+     * Get version of this artifact.
+     * 
+     * @return artifact version, never {@code null}.
+     */
     String getVersion();
 
+    /**
+     * Get local path of artifact file or {@code null} if artifact is not resolved (doesn't have associated artifact
+     * file).
+     * 
+     * @return artifact file path, can be {@code null}.
+     */
     Path getPath();
 
+    /**
+     * Set artifact version.
+     * <p>
+     * Since artifacts are immutable, this method returns a new object and leaves the original unmodified.
+     * 
+     * @param version the new artifact version to set
+     * @return copy of artifact with the new version set
+     */
     Artifact setVersion( String version );
 
+    /**
+     * Set artifact path.
+     * <p>
+     * Since artifacts are immutable, this method returns a new object and leaves the original unmodified.
+     * 
+     * @param path the new artifact path to set
+     * @return copy of artifact with the new path set
+     */
     Artifact setPath( Path path );
 }
