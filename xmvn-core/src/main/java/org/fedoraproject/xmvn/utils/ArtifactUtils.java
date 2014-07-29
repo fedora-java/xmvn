f83d21d4be212a49f5040a3d90862beb6919e0b6
Mikolaj Izdebski <mizdebsk@redhat.com>
2014-07-29 10:34:29 +0200

    Mark dummy artifacts as deprecated

--- xmvn-core/src/main/java/org/fedoraproject/xmvn/utils/ArtifactUtils.java
+++ xmvn-core/src/main/java/org/fedoraproject/xmvn/utils/ArtifactUtils.java
@@ -58,12 +58,14 @@ public class ArtifactUtils
     /**
      * Dummy artifact. Any dependencies on this artifact will be removed during model validation.
      */
+    @Deprecated
     public static final Artifact DUMMY = new DefaultArtifact( "org.fedoraproject.xmvn:xmvn-void:SYSTEM" );
 
     /**
      * The same as {@code DUMMY}, but in JPP style. Any dependencies on this artifact will be removed during model
      * validation.
      */
+    @Deprecated
     public static final Artifact DUMMY_JPP = new DefaultArtifact( "JPP/maven:empty-dep:SYSTEM" );
 
     /**
