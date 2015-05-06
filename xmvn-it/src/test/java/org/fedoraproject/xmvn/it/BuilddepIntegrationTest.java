472aa0852d6a52d1f5b6995bc19e1fd00a730baf
Mikolaj Izdebski <mizdebsk@redhat.com>
2015-05-06 13:19:45 +0200

    Add reproducer for rhbz#1217422

---
+++ xmvn-it/src/test/java/org/fedoraproject/xmvn/it/BuilddepIntegrationTest.java
@@ -0,0 +1,63 @@
+/*-
+ * Copyright (c) 2015 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.fedoraproject.xmvn.it;
+
+import static org.custommonkey.xmlunit.XMLAssert.assertXMLEqual;
+import static org.custommonkey.xmlunit.XMLUnit.setIgnoreWhitespace;
+import static org.junit.Assert.assertTrue;
+
+import java.io.StringReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * @author Mikolaj Izdebski
+ */
+public class BuilddepIntegrationTest
+    extends AbstractIntegrationTest
+{
+    private void assertBuilddepEqual( String... expected )
+        throws Exception
+    {
+        setIgnoreWhitespace( true );
+        Path builddepPath = Paths.get( ".xmvn-builddep" );
+        assertTrue( Files.isRegularFile( builddepPath ) );
+
+        StringBuilder sb = new StringBuilder();
+        for ( String s : expected )
+            sb.append( s );
+        assertXMLEqual( new StringReader( sb.toString() ), Files.newBufferedReader( builddepPath ) );
+    }
+
+    @Test
+    @Ignore
+    public void testBuilddepExpandVariables()
+        throws Exception
+    {
+        performTest( "org.fedoraproject.xmvn:xmvn-mojo:builddep" );
+
+        assertBuilddepEqual( "<dependencies>", //
+                             "  <dependency>", //
+                             "    <groupId>gid</groupId>", //
+                             "    <artifactId>dep</artifactId>", //
+                             "  </dependency>", //
+                             "</dependencies>" );
+    }
+}
