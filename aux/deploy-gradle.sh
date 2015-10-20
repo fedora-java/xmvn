c02dfd6c517065590fdeee1e7910b3305ee55e33
Mikolaj Izdebski <mizdebsk@redhat.com>
2015-10-20 11:16:40 +0200

    Implement XMvnGradlePlugin

--- aux/deploy-gradle.sh
+++ aux/deploy-gradle.sh
@@ -7,7 +7,7 @@ test -n "$version"
 wget http://services.gradle.org/distributions/gradle-$version-bin.zip
 unzip gradle-$version-bin.zip
 
-for artifactId in gradle-base-services gradle-core gradle-dependency-management gradle-resources; do
+for artifactId in gradle-base-services gradle-base-services-groovy gradle-core gradle-dependency-management gradle-resources; do
     file=$PWD/gradle-$version/lib/$artifactId-$version.jar
     test -e $file || file=$PWD/gradle-$version/lib/plugins/$artifactId-$version.jar
 
