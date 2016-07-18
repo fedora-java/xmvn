5ad70d4eadbe64e4e110cdb820a79a3363492ed7
Mikolaj Izdebski <mizdebsk@redhat.com>
2016-07-18 09:00:22 +0200

    [it] Add builddep test for inheritance from reactor POM
    Dependency was inherited from another POM, which is part of reactor.

--- xmvn-it/src/test/java/org/fedoraproject/xmvn/it/BuilddepIntegrationTest.java
+++ xmvn-it/src/test/java/org/fedoraproject/xmvn/it/BuilddepIntegrationTest.java
@@ -132,4 +132,18 @@ public class BuilddepIntegrationTest
                              "  </dependency>", //
                              "</dependencies>" );
     }
+
+    @Test
+    public void testBuilddepSubmoduleInheritance()
+        throws Exception
+    {
+        performTest( "verify", "org.fedoraproject.xmvn:xmvn-mojo:2.6.0-SNAPSHOT:builddep" );
+
+        assertBuilddepEqual( "<dependencies>", //
+                             "  <dependency>", //
+                             "    <groupId>org.codehaus.plexus</groupId>", //
+                             "    <artifactId>plexus-component-metadata</artifactId>", //
+                             "  </dependency>", //
+                             "</dependencies>" );
+    }
 }
