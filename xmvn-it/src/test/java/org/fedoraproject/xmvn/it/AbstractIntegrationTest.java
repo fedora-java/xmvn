146c0c4b85db2c8e4ac33746002117ae35688e2c
Mikolaj Izdebski <mizdebsk@redhat.com>
2017-12-08 13:44:16 +0100

    [it] Inhibit calling System.exit()

--- xmvn-it/src/test/java/org/fedoraproject/xmvn/it/AbstractIntegrationTest.java
+++ xmvn-it/src/test/java/org/fedoraproject/xmvn/it/AbstractIntegrationTest.java
@@ -21,9 +21,11 @@ import java.nio.file.Files;
 import java.nio.file.LinkOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.security.Permission;
 import java.util.stream.Stream;
 
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Rule;
@@ -147,5 +149,50 @@ public abstract class AbstractIntegrationTest
         throws Exception
     {
         return Files.lines( baseDir.resolve( STDERR ) );
+    }
+
+    /**
+     * @author Mikolaj Izdebski
+     */
+    public static class SystemExit
+        extends SecurityException
+    {
+        private static final long serialVersionUID = 1;
+
+        private final int status;
+
+        public SystemExit( int status )
+        {
+            this.status = status;
+        }
+
+        public int getStatus()
+        {
+            return status;
+        }
+    }
+
+    @BeforeClass
+    public static void inhibitSystemExit()
+    {
+        System.setSecurityManager( new SecurityManager()
+        {
+            @Override
+            public void checkExit( int status )
+            {
+                throw new SystemExit( status );
+            }
+
+            @Override
+            public void checkPermission( Permission perm )
+            {
+            }
+        } );
+    }
+
+    @AfterClass
+    public static void restoreSystemExit()
+    {
+        System.setSecurityManager( null );
     }
 }
