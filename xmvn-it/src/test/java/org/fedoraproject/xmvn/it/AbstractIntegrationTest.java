5f4c6c3e4b8f2abb27ddb4a04ae6f675f7325d58
Mikolaj Izdebski <mizdebsk@redhat.com>
2019-06-14 13:04:11 +0200

    Release version 3.1.0

--- xmvn-it/src/test/java/org/fedoraproject/xmvn/it/AbstractIntegrationTest.java
+++ xmvn-it/src/test/java/org/fedoraproject/xmvn/it/AbstractIntegrationTest.java
@@ -91,7 +91,7 @@ public abstract class AbstractIntegrationTest
     public void createBaseDir()
         throws Exception
     {
-        mavenHome = Paths.get( "../dependency/xmvn-3.1.0-SNAPSHOT" ).toAbsolutePath();
+        mavenHome = Paths.get( "../dependency/xmvn-3.1.0" ).toAbsolutePath();
 
         baseDir = Paths.get( "." ).toRealPath();
         delete( baseDir );
