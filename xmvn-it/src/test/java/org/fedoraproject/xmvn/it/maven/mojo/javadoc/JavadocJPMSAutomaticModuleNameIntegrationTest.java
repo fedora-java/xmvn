a8a3da99e696e7d42597a0a8b30b81d1cbdfb4de
Marián Konček <marian.koncek@mailbox.org>
2023-03-16 16:21:13 +0100

    Add an integration test for javadoc generation with Automatic-Module-Name

---
+++ xmvn-it/src/test/java/org/fedoraproject/xmvn/it/maven/mojo/javadoc/JavadocJPMSAutomaticModuleNameIntegrationTest.java
@@ -0,0 +1,48 @@
+/*-
+ * Copyright (c) 2015-2023 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.fedoraproject.xmvn.it.maven.mojo.javadoc;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.junit.jupiter.api.Test;
+
+import org.fedoraproject.xmvn.it.maven.mojo.AbstractMojoIntegrationTest;
+
+/**
+ * Integration tests for javadoc MOJO.
+ * 
+ * @author Marian Koncek
+ */
+public class JavadocJPMSAutomaticModuleNameIntegrationTest
+    extends AbstractMojoIntegrationTest
+{
+    @Test
+    public void testJavadocJPMSAutomaticModuleName()
+        throws Exception
+    {
+        assumeTrue( getJavaVersion() >= 9 );
+        performMojoTest( "verify", "javadoc" );
+
+        assertTrue( Files.isDirectory( Paths.get( "target/xmvn-apidocs" ) ) );
+        assertTrue( Files.isRegularFile( Paths.get( "target/xmvn-apidocs/A.html" ) ) );
+        assertTrue( Files.isSymbolicLink( Paths.get( ".xmvn/apidocs" ) ) );
+        assertTrue( Files.isSameFile( Paths.get( ".xmvn/apidocs" ), Paths.get( "target/xmvn-apidocs" ) ) );
+    }
+}
