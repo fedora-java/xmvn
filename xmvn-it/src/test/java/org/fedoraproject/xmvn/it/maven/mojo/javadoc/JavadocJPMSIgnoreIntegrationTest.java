a1403c2327130c75513e50b4c0816d073c35ca2c
Mikolaj Izdebski <mizdebsk@redhat.com>
2023-03-18 09:30:53 +0100

    [it] Add test for Javadoc ignoring JPMS

---
+++ xmvn-it/src/test/java/org/fedoraproject/xmvn/it/maven/mojo/javadoc/JavadocJPMSIgnoreIntegrationTest.java
@@ -0,0 +1,43 @@
+/*-
+ * Copyright (c) 2015-2023 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.fedoraproject.xmvn.it.maven.mojo.javadoc;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+
+import org.junit.jupiter.api.Test;
+
+import org.fedoraproject.xmvn.it.maven.mojo.AbstractMojoIntegrationTest;
+
+/**
+ * Integration tests for javadoc MOJO.
+ * 
+ * @author Mikolaj Izdebski
+ */
+public class JavadocJPMSIgnoreIntegrationTest
+    extends AbstractMojoIntegrationTest
+{
+    @Test
+    public void testJavadocJPMSIgnore()
+        throws Exception
+    {
+        assumeTrue( getJavaVersion() >= 9 );
+        expectFailure();
+        performMojoTest( "verify", "-Dxmvn.javadoc.ignoreJPMS", "javadoc" );
+
+        assertTrue( getStdout().anyMatch( "[INFO] Ignoring JPMS according to configuration (xmvn.javadoc.ignoreJPMS property)"::equals ) );
+    }
+}
