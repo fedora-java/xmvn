f0e9eba024e730e6a3ddee3eb1e7152a59a5a7c7
Chris Kelley <82874386+ckelleyRH@users.noreply.github.com>
2024-06-14 10:06:16 +0100

    Fix typo in JavadocMojo

--- xmvn-mojo/src/main/java/org/fedoraproject/xmvn/mojo/JavadocMojo.java
+++ xmvn-mojo/src/main/java/org/fedoraproject/xmvn/mojo/JavadocMojo.java
@@ -218,7 +218,7 @@ public class JavadocMojo
         long nNonAutomatic = nModular - nAutomatic;
         if ( nAutomatic > 0 && nNonAutomatic > 0 )
         {
-            logger.warn( "Found {} non-automacit modules (with module-info) and {} automatic modules."
+            logger.warn( "Found {} non-automatic modules (with module-info) and {} automatic modules."
                 + " Mixing automatic and non-automatic modules is not supported. Javadoc is likely to fail."
                 + " For more info see debug output.", nNonAutomatic, nAutomatic );
         }
