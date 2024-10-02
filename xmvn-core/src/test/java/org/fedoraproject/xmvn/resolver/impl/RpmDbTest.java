b59ee70754fa9f7102b17cba6343c24e9733cdd1
Mikolaj Izdebski <mizdebsk@redhat.com>
2024-10-02 22:10:09 +0200

    Fix RpmDb to work with current RPM versions

---
+++ xmvn-core/src/test/java/org/fedoraproject/xmvn/resolver/impl/RpmDbTest.java
@@ -0,0 +1,50 @@
+/*-
+ * Copyright (c) 2024 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.fedoraproject.xmvn.resolver.impl;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+/** @author Mikolaj Izdebski */
+public class RpmDbTest {
+    @TempDir
+    private Path tempDir;
+
+    @BeforeAll
+    public static void ensureRpmIsPresent() {
+        assumeTrue(Files.isExecutable(Path.of("/usr/bin/rpm")));
+    }
+
+    @Test
+    public void testRpmPathLookupSuccess() {
+        RpmDb db = new RpmDb();
+        String pkg = db.lookupPath("/usr/bin");
+        assertThat(pkg).matches("filesystem (.*)");
+    }
+
+    @Test
+    public void testRpmPathLookupFail() {
+        RpmDb db = new RpmDb();
+        String pkg = db.lookupPath(tempDir.toString());
+        assertThat(pkg).isNull();
+    }
+}
